25d0f50bf6ca11cb97035be7b108127afd369303
Gulya Akhtyamova <gulya.nadym@gmail.com>
2020-07-16 12:56:23 +0300

    Initial Commit

---
+++ TreeStructure/TreeStructure.h
@@ -0,0 +1,19 @@
+//
+//  TreeStructure.h
+//  TreeStructure
+//
+//  Created by Gulya Boiko on 7/16/20.
+//  Copyright © 2020 com.gulya.boiko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TreeStructure.
+FOUNDATION_EXPORT double TreeStructureVersionNumber;
+
+//! Project version string for TreeStructure.
+FOUNDATION_EXPORT const unsigned char TreeStructureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TreeStructure/PublicHeader.h>
+
+
